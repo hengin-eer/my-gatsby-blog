80037e419b496cc2628324cb38eecef0d8d72aad
hengin-eer <hirokix0608@gmail.com>
2023-05-08 10:17:33 +0900

    🎉

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,10 +3,38 @@
  *
  * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-config/
  */
+require('dotenv').config({
+	path: `.env.${process.env.NODE_ENV}`,
+})
 
 /**
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
-  plugins: [],
+	siteMetadata: {
+		title: "Hengineer.tech",
+		description: "Hengineerのポートフォリオ & ブログ",
+		author: "Engineer X"
+	},
+	/* Your site config here */
+	plugins: [
+		{
+			resolve: `gatsby-source-contentful`,
+			options: {
+				spaceId: process.env.GATSBY_CONTENTFUL_SPACE_ID,
+				accessToken: process.env.GATSBY_CONTENTFUL_API_KEY
+			}
+		},
+		{
+			resolve: `gatsby-transformer-remark`,
+			options: {
+				commonmark: true,
+				footnotes: true,
+				pedantic: true,
+				gfm: true,
+				plugins: [],
+			},
+		},
+		`gatsby-plugin-image`,
+	],
 }
