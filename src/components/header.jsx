cb3ae6bab3c552204d5d9348b3e9a4f2cbb31ad1
hengin-eer <hirokix0608@gmail.com>
2023-05-08 10:18:30 +0900

    🎉

---
+++ src/components/header.jsx
@@ -0,0 +1,15 @@
+// src/components/header.js
+import React from "react"
+import { Link } from "gatsby"
+import LOGO_IMG from "../../static/logo.png"
+import "../styles/header.css"
+
+export default function Header() {
+	return (
+		<header>
+			<Link to="/">
+				<img src={LOGO_IMG} className="logo-image" alt="logo"></img>
+			</Link>
+		</header>
+	)
+}
