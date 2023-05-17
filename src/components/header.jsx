8c73576373569a929ed36da6463c32cf095f90d3
hengin-eer <hirokix0608@gmail.com>
2023-05-17 09:21:14 +0900

    feat: #3 SNSリンクの作成、react-iconsの読み込み

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,14 +2,25 @@
 import React from "react"
 import { Link } from "gatsby"
 import LOGO_IMG from "../../static/logo.png"
+import { FiGithub, FiTwitter } from "react-icons/fi"
 import "../styles/header.css"
 
 export default function Header() {
+	const myTwitter = "https://twitter.com/hengineer_"
+	const myGitHub = "https://github.com/hengin-eer"
 	return (
 		<header>
 			<Link to="/">
 				<img src={LOGO_IMG} className="logo-image" alt="logo"></img>
 			</Link>
+			<ul className="header-nav">
+				<li>
+					<Link to={myTwitter}><FiTwitter /></Link>
+				</li>
+				<li>
+					<Link to={myGitHub}><FiGithub /></Link>
+				</li>
+			</ul>
 		</header>
 	)
 }
