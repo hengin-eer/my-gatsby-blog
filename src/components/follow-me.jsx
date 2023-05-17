abde930027059c050e7a04bc2dee1aa1475323c6
hengin-eer <hirokix0608@gmail.com>
2023-05-17 15:15:42 +0900

    feat: #7 Twitterの技術アカウント、説明の追加

--- src/components/follow-me.jsx
+++ src/components/follow-me.jsx
@@ -1,23 +1,33 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import "../styles/follow-me.css"
 import { FiGithub, FiTwitter } from "react-icons/fi"
 
 export default function FollowMe() {
-	const myTwitter = "https://twitter.com/hengineer_"
+	const myTwitter = "https://twitter.com/tim_daik"
 	const myGitHub = "https://github.com/hengin-eer"
+	const techTwitter = "https://twitter.com/hengineer_"
 
 	return (
-		<div className='follow-me'>
+		<div className='follow'>
 			<div className="follow-title">\ Follow Me /</div>
-			<ul>
+			<ul className='follow-links'>
 				<li>
-					<Link to={myTwitter}>
+					<Link to={techTwitter}>
 						<FiTwitter />
+						<p>技術垢</p>
 					</Link>
 				</li>
 				<li>
 					<Link to={myGitHub}>
 						<FiGithub />
+						<p>開発垢</p>
+					</Link>
+				</li>
+				<li>
+					<Link to={myTwitter}>
+						<FiTwitter />
+						<p>日常垢</p>
 					</Link>
 				</li>
 			</ul>
